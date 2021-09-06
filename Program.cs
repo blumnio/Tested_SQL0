251950b73a3a0b692006f493ce1fda0d2902d302
Gregory <grigorij.marinckin@yandex.ru>
2021-09-06 19:22:28 +0300

    Connection and Output information

--- Program.cs
+++ Program.cs
@@ -10,29 +10,22 @@ namespace Tested_SQL
         static async Task Main(string[] args)
         {
             string connectionString = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;";
-
-            //Create connection
-            SqlConnection connection = new SqlConnection(connectionString);
-            try
+            //To save space and code volume, instead of explicitly closing the connection, you can also use the "using" construct, which automatically closes the connection
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                //Open connection
                 await connection.OpenAsync();
                 Console.WriteLine("Connection is open");
+                //Output of connection information
+                Console.WriteLine("Connection Properties:");
+                Console.WriteLine($"\tConnection string: {connection.ConnectionString}");
+                Console.WriteLine($"\tDataBase: {connection.Database}");
+                Console.WriteLine($"\tServer: {connection.DataSource}");
+                Console.WriteLine($"\tServer version: {connection.ServerVersion}");
+                Console.WriteLine($"\tState: {connection.State}");
+                Console.WriteLine($"\tWorkStationID: {connection.WorkstationId}");
             }
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                //If connection is open
-                if (connection.State == ConnectionState.Open)
-                { //We close conneetion
-                    await connection.CloseAsync();
-                    Console.WriteLine("Connection is close...");
-                }
-            }
-            Console.WriteLine("The program has finished working.");
+            Console.WriteLine("Connection is close...");
+            Console.WriteLine("The program has finished working");
             Console.Read();
         }
     }
